683e552a20e02db1d36ec85814a9a2810a28436a
Aaditya-Chunekar <aadityachunekar@gmail.com>
2026-07-05 19:29:27 +0530

    dayOnePartTwo

--- day1.py
+++ day1.py
@@ -1,24 +1,29 @@
 b,l,r=50,0,99
 ans=0
+ansb=0
 with open("day1.ip","r") as f:
     for line in f:
         # print('-'*10)
         v=int(line[1:])
         # print(line, v)
-        if(line[0]=="L"):
-            b-=v
-            if(b<l):
-                b%=100
-                
-        else:
-            b+=v
-            if(b>r):
-                b%=100
+        if(line[0]=="R"):
+            total = b + v
+            # MAKING A SEPARATE total VARIABLE was necessary
+            ansb += total // 100
+            b = total % 100
+        else: 
+            # "this is a very important block"
+            first = b if b != 0 else 100
+            if v >= first:
+                ansb += (v - first) // 100 + 1
+            b = (b - v) % 100
         if(b==0):
            ans+=1
-print(ans)
+        # print("b",b)
+        #    ansb+=1
+print(ans,ansb)
 
 '''
  after the granting of star, 
  git add .; git commit -m "dayOne"; git push
- '''
+'''
