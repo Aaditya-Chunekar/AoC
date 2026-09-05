af65b22892978f7d98ea72640386194f4684494a
Aaditya-Chunekar <aadityachunekar@gmail.com>
2026-09-05 10:40:32 +0530

    [2015] dayOnePartTwo

--- 2015/day1/day1.py
+++ 2015/day1/day1.py
@@ -1,5 +1,17 @@
 ans=0
+ansTwo=1
+loc=0
 with open("day1.ip",'r') as f:
     for line in f:
         ans+=line.count('(')-line.count(')')
+        for c in line:
+            if c=='(':
+                loc+=1
+            else:
+                loc-=1
+            if loc==-1:
+                print("ansTwo",ansTwo)
+                break
+            ansTwo+=1
+
 print(ans)
